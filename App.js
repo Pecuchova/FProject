e355fb6608a2d7c3ea0bab646e96f894c2b53e57
Pecuchova <jpecuchova96@gmail.com>
2024-09-30 20:57:11 +0200

    základy view component

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 import TextComponent from './components/Text/TextComponent';
 
 const App = () => {
   return (
     <SafeAreaView>
+      <View style={{ backgroundColor: "blue", height: 100}}> 
+        <Text>Header</Text>
+      </View>
+      <View style={{ backgroundColor: "grey"}}>
       <TextComponent />
+      <TextComponent />
+      <TextComponent />
+      </View>
+      <View>
+        <Text>Footer</Text>
+      </View>
     </SafeAreaView>
   );
 };
