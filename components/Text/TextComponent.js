82af0fd0551d7c56407056b1dd1bcd3fde4b17e8
Pecuchova <jpecuchova96@gmail.com>
2024-09-30 20:46:42 +0200

    stylesheet style

--- components/Text/TextComponent.js
+++ components/Text/TextComponent.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Text} from "react-native";
+import Style from "./Style";
 
 const TextComponent = () => {
-    return <Text style={{color: "orange"}}> Hello, React Native World! </Text>;
+    return <Text style={[Style.text, Style.text1, {color: "green"}]}> Hello, React Native World! </Text>;
 };
 
 export default TextComponent;
