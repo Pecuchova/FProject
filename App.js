1320251396f985f7e8b30eb2baba8fcd7588d97f
Pecuchova <jpecuchova96@gmail.com>
2024-10-01 00:01:33 +0200

    useState hook

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Image} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
 
 
 const App = () => {
-  const [imageSource, setImageSource] = useState({uri:"http://example.com"});
+  const [text, setText] = useState("Hello, World!")
   return (
     <SafeAreaView>
-      <Image 
-      source={require("./assets/images/cake.png")} 
-      style={{width: "100%", height: "100%", backgroundColor: "red"}} 
-      resizeMode={"repeat"}
-      onError = {() => {
-        console.log("Error has been occured while loading an image");
-        setImageSource(require("./assets/images/cake.png"))
-      }}
-      />
-      <Image 
-      source={{ 
-        uri: 'https://static01.nyt.com/images/2023/10/27/multimedia/27cakerex-plzm/27cakerex-plzm-threeByTwoMediumAt2X.jpg?width=1280&quality=75&auto=webp'
-        }} 
-        style={{width: 100, height: 100}} 
-        />
+     <Text> Hello World! </Text>
+     <Text
+        onPress={() => setText("Nový stav.")} >
+          {text}
+     </Text>
     </SafeAreaView>
   );
 };
