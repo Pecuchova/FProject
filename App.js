79ba89c23be38638dc270cbd79bd1ea8d53a73f9
Pecuchova <jpecuchova96@gmail.com>
2024-09-30 23:38:38 +0200

    image component

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import TextComponent from './components/Text/TextComponent';
+import React, {useState} from 'react';
+import {SafeAreaView, Image} from 'react-native';
 
 
 const App = () => {
+  const [imageSource, setImageSource] = useState({uri:"http://example.com"});
   return (
     <SafeAreaView>
-      <TextComponent />
+      <Image 
+      source={require("./assets/images/cake.png")} 
+      style={{width: "100%", height: "100%", backgroundColor: "red"}} 
+      resizeMode={"repeat"}
+      onError = {() => {
+        console.log("Error has been occured while loading an image");
+        setImageSource(require("./assets/images/cake.png"))
+      }}
+      />
+      <Image 
+      source={{ 
+        uri: 'https://static01.nyt.com/images/2023/10/27/multimedia/27cakerex-plzm/27cakerex-plzm-threeByTwoMediumAt2X.jpg?width=1280&quality=75&auto=webp'
+        }} 
+        style={{width: 100, height: 100}} 
+        />
     </SafeAreaView>
   );
 };
 
 export default App;
+
+
+
+
+
+
--- components/Text/TextComponent.js
+++ components/Text/TextComponent.js
@@ -15,3 +15,5 @@ const TextComponent = () => {
 
 
 export default TextComponent;
+
+
