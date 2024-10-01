4d26114bb76522334a6dea5b72bb1bad3fec4ce3
Pecuchova <jpecuchova96@gmail.com>
2024-10-01 10:49:04 +0200

    class based vs functional components

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
+import TextComponent from './components/Text/TextComponent';
 
-import {Button, SafeAreaView, Text} from 'react-native';
-import useToggle from './customHooks/useToggle'; // Import the custom hook
 
   // Render the App component
-    const App = () => {
-      // Call the custom hook and get the current value of the boolean state and the toggle function to update the state
-      const [isOn, toggleIsOn] = useToggle(false);    
-    
-      return (
-        <SafeAreaView>
-          <Text>{isOn ? 'ON' : 'OFF'}</Text>
-          {/* Display the current value of the boolean state */}
-          <Button title={'Toggle'} onPress={toggleIsOn} />
-          {/* Render a button that calls the toggle function when pressed */}
-        </SafeAreaView>
-      );
-    };
+  const App = () => {
+    return (
+      <SafeAreaView>
+        <TextComponent name={'Janka'} />
+      </SafeAreaView>
+    );
+  };
 
 // Export the App component as the default export of the module.
 export default App;
--- components/Text/TextComponent.js
+++ components/Text/TextComponent.js
@@ -1,18 +1,71 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import {Text} from "react-native";
-import Style from "./Style";
+import style from './Style';
 
-const TextComponent = () => {
-    const handleTextClick = () => {
-        alert("The text has been clicked!")
-    }
+const TextComponent = props => {
+    const [fullName, setFullName] = useState('');
+  
+    useEffect(() => {
+      //It is called right after the component is rendered
+      //Perform some setup for example fetching data from API
+      console.log('Our component has been mounted');
+      return () => {
+        console.log('Component is now being destroyed');
+      };
+    }, []);
+  
+    useEffect(() => {
+      console.log('The value of full name has been changed');
+    }, [fullName]);
+
     return (
-        <Text style={[Style.text, Style.text1, {color: "green"}]} onPress={() => handleTextClick()}>
-         Hello, React Native World! 
-         </Text>
-    );
-};
-
+        <Text
+          style={[style.text, style.text1, {color: 'green'}]}
+          onPress={() => setFullName('Janka Pecuchova')}>
+          Hello, {props.name}! I know that your full name is {fullName}
+        </Text>
+      );
+    };
+    
+    /**
+     * ---------------------------- Uncomment for Class Based Component -----------------------------
+     class TextComponent extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          fullName: props.name + ' Pecuchova',
+        };
+      }
+      componentDidMount() {
+        //It is called right after the component is rendered
+        //Perform some setup for example fetching data from API
+        console.log('Our component has been mounted');
+      }
+      componentDidUpdate(prevProps, prevState) {
+        //Called when state or props of the application is updated
+        console.log(
+          'The state of the application has been changed',
+          prevState,
+          this.state,
+        );
+      }
+      componentWillUnmount() {
+        console.log('Component is now being destroyed');
+      }
+      render() {
+        return (
+          <Text
+            style={[style.text, style.text1, {color: 'green'}]}
+            onPress={() =>
+              this.setState({...this.state, ...{fullName: 'Janka P.'}})
+            }>
+            Hello, {this.props.name}! I know that your full name is{' '}
+            {this.state.fullName}
+          </Text>
+        );
+      }
+    }
+     */
 
 export default TextComponent;
 
