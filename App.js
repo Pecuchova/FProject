53875e3d34c52893131b0a012d65aa08fb93fd56
Pecuchova <jpecuchova96@gmail.com>
2024-10-01 09:03:17 +0200

    useEffect hook

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 
 const App = () => {
-  const [text, setText] = useState("Hello, World!")
+  const [text, setText] = useState(0);
+  useEffect( () => {
+    console.log("Text sa zmenil:");
+   }, [text]);
+  
+
+  useEffect( () => {
+    console.log("Komponent bol renderovaný!");
+   }, []);
+
   return (
     <SafeAreaView>
-     <Text> Hello World! </Text>
      <Text
-        onPress={() => setText("Nový stav.")} >
+        onPress={() => setText( text + 1)} >
           {text}
      </Text>
     </SafeAreaView>
