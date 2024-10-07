e78be367a260364fa80994c6fbded88065fad09e
Pecuchova <jpecuchova96@gmail.com>
2024-10-07 21:56:44 +0200

    FontAwesome Icons

--- App.js
+++ App.js
@@ -9,53 +9,56 @@ import {
   Text,
   View,
 } from 'react-native';
+// Importy pre FontAwesome
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
-  // Deklarácia stavových premenných na uloženie hodnôt emailu a hesla, ktoré zadal používateľ
+  // Deklarácia stavových premenných pre uchovávanie hodnôt emailu a hesla, ktoré zadá používateľ
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // Deklarácia stavovej premennej na uloženie hodnoty, či má používateľ zostať prihlásený alebo nie
+  // Deklarácia stavovej premennej pre uchovávanie hodnoty, či používateľ má zostať prihlásený
   const [shouldKeepLoggedIn, setShouldKeepLoggedIn] = useState(true);
   return (
     <SafeAreaView>
       <ScrollView>
         <TextInput
-          // Hodnota TextInput na stavovú premennú email
+          // Nastavenie hodnoty TextInput na stavovú premennú email
           value={email}
-          // KeyboardType na 'email-address' pre zobrazenie rozloženia klávesnice pre email
+          // Nastavenie keyboardType na 'email-address' pre zobrazenie klávesnice na zadávanie emailov
           keyboardType={'email-address'}
-          // Štýl TextInput, aby mal okraj, padding a zaoblené rohy
+          // Nastavenie štýlu TextInput tak, aby mal okraj, padding a zaoblené rohy
           style={{borderWidth: 1, borderRadius: 4, padding: 10}}
-          // Text zástupného symbolu (placeholder), ktorý používateľa vyzve na zadanie emailu
-          placeholder={'Prosím zadajte svoj email'}
-          // Funkciu onChangeText na aktualizáciu stavovej premennej email s novou hodnotou zadanou používateľom
+          // Nastavenie zástupného textu (placeholder) pre emailové pole
+          placeholder={'Prosím, zadajte svoj email'}
+          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej email novou hodnotou zadanou používateľom
           onChangeText={value => {
             setEmail(value);
           }}
         />
         <TextInput
-          // Hodnota TextInput na stavovú premennú password
+          // Nastavenie hodnoty TextInput na stavovú premennú heslo
           value={password}
-          // SecureTextEntry na true, aby sa zadaný text zobrazoval ako bodky (pre heslá)
+          // Nastavenie secureTextEntry na true, aby sa skryté znaky zobrazovali ako body (pre heslá)
           secureTextEntry={true}
-          // Štýl TextInput, aby mal okraj, padding a zaoblené rohy
+          // Nastavenie štýlu TextInput tak, aby mal okraj, padding a zaoblené rohy
           style={{borderWidth: 1, borderRadius: 4, padding: 10}}
-          // Text zástupného symbolu (placeholder), ktorý používateľa vyzve na zadanie hesla
-          placeholder={'Prosím zadajte svoje heslo'}
-          // Funkcia onChangeText na aktualizáciu stavovej premennej password s novou hodnotou zadanou používateľom
+          // Nastavenie zástupného textu (placeholder) pre heslo
+          placeholder={'Prosím, zadajte svoje heslo'}
+          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej heslo novou hodnotou zadanou používateľom
           onChangeText={value => {
             setPassword(value);
           }}
         />
         <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-          {/*-- Switch komponent na rozhodnutie, či chce používateľ zostať prihlásený alebo nie --*/}
+          {/* -- Komponent Switch pre rozhodnutie, či má používateľ zostať prihlásený alebo nie -- */}
           <Switch
-            value={shouldKeepLoggedIn} // Aktuálna hodnota switch (true - zapnuté, false - vypnuté)
-            onValueChange={value => setShouldKeepLoggedIn(value)} // Pri zmene hodnoty switchu sa aktualizuje aj stavová premenná
+            value={shouldKeepLoggedIn} //Aktuálna hodnota prepínača (true - zapnuté, false - vypnuté)
+            onValueChange={value => setShouldKeepLoggedIn(value)} //Po zmene hodnoty prepínača sa obnoví stavová premenná
           />
           <Text>Zapamätať prihlásenie</Text>
         </View>
-        {/* -- Tlačidlo komponent nepodporuje zmenu farby pozadia, preto použijeme Pressable
+        {/* -- Button komponent nemá podporu pre background color, preto používame Pressable (TouchableOpacity)
             <Button
               title={'Odoslať'}
               color={'red'}
@@ -63,25 +66,45 @@ const App = () => {
             />
         */}
         <TouchableOpacity
-          // Štýl Pressable na čierne pozadie
-          // Opacity na 0.5, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
+          // Nastavenie štýlu TouchableOpacity na čierne pozadie
+          // Nastavenie opacity na 0.5, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
           style={[
             {backgroundColor: 'black'},
             (email.length === 0 || password.length < 8) && {opacity: 0.5},
           ]}
-          // Pressable, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
+          // Deaktivovanie tlačidla, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
           disabled={email.length === 0 || password.length < 8}
-          // Funkcia onPress, ktorá vypíše email, heslo a hodnotu "nechať prihláseného" do konzoly
+          // Nastavenie funkcie onPress na výpis emailu, hesla a hodnoty "nechať prihláseného" do konzoly
           onPress={() => {
             console.log('kliknuté');
             console.log(email, password, shouldKeepLoggedIn);
           }}>
-          <Text style={{color: 'white', textAlign: 'center', padding: 10}}>
-            Odoslať
-          </Text>
+          <View
+            // Nastavenie štýlu pre View tak, aby mal flex hodnotu 1,
+            // zarovnanie komponentov v riadku (row) a centrované vertikálne aj horizontálne zarovnanie
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text
+              // Nastavenie štýlu Text tak, aby mal bielu farbu, zarovnanie na stred a padding
+              style={{color: 'white', textAlign: 'center', padding: 10}}>
+              Odoslať
+            </Text>
+            <FontAwesomeIcon
+              // Nastavenie ikony komponentu FontAwesomeIcon na ikonu faCheck
+              icon={faCheck}
+              // Nastavenie farby ikony na bielu
+              style={{color: 'white'}}
+            />
+          </View>
         </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+
 export default App;
