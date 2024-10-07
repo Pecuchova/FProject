78c7829ea67ca09c2634f15177b93fed8d3668ea
Pecuchova <jpecuchova96@gmail.com>
2024-10-07 21:37:59 +0200

    Switch Component

--- App.js
+++ App.js
@@ -4,70 +4,82 @@ import {
   ScrollView,
   TextInput,
   // Button,
-  Pressable,
+  TouchableOpacity,
+  Switch,
   Text,
+  View,
 } from 'react-native';
 
 const App = () => {
-  // Deklarácia stavových premenných na uloženie hodnôt emailu a hesla zadaných používateľom
+  // Deklarácia stavových premenných na uloženie hodnôt emailu a hesla, ktoré zadal používateľ
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // Deklarácia stavovej premennej na uloženie hodnoty, či má používateľ zostať prihlásený alebo nie
+  const [shouldKeepLoggedIn, setShouldKeepLoggedIn] = useState(true);
   return (
     <SafeAreaView>
       <ScrollView>
         <TextInput
-          // Nastavenie hodnoty TextInput na stavovú premennú email
+          // Hodnota TextInput na stavovú premennú email
           value={email}
-          // Nastavenie keyboardType na 'email-address' pre zobrazenie klávesnice optimalizovanej pre zadávanie emailu
+          // KeyboardType na 'email-address' pre zobrazenie rozloženia klávesnice pre email
           keyboardType={'email-address'}
-          // Nastavenie štýlu TextInput s ohraničením, paddingom a zaoblenými rohmi
+          // Štýl TextInput, aby mal okraj, padding a zaoblené rohy
           style={{borderWidth: 1, borderRadius: 4, padding: 10}}
-          // Nastavenie textu v placeholderi, ktorý vyzýva používateľa na zadanie emailu
-          placeholder={'Prosím, zadajte svoj email'}
-          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej email novou hodnotou zadanou používateľom
+          // Text zástupného symbolu (placeholder), ktorý používateľa vyzve na zadanie emailu
+          placeholder={'Prosím zadajte svoj email'}
+          // Funkciu onChangeText na aktualizáciu stavovej premennej email s novou hodnotou zadanou používateľom
           onChangeText={value => {
             setEmail(value);
           }}
         />
         <TextInput
-          // Nastavenie hodnoty TextInput na stavovú premennú password (heslo)
+          // Hodnota TextInput na stavovú premennú password
           value={password}
-          // Nastavenie secureTextEntry na true, aby sa zadaný text zobrazoval ako bodky (pre heslá)
+          // SecureTextEntry na true, aby sa zadaný text zobrazoval ako bodky (pre heslá)
           secureTextEntry={true}
-          // Nastavenie štýlu TextInput s ohraničením, paddingom a zaoblenými rohmi
+          // Štýl TextInput, aby mal okraj, padding a zaoblené rohy
           style={{borderWidth: 1, borderRadius: 4, padding: 10}}
-          // Nastavenie textu v placeholderi, ktorý vyzýva používateľa na zadanie hesla
-          placeholder={'Prosím, zadajte svoje heslo'}
-          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej password novou hodnotou zadanou používateľom
+          // Text zástupného symbolu (placeholder), ktorý používateľa vyzve na zadanie hesla
+          placeholder={'Prosím zadajte svoje heslo'}
+          // Funkcia onChangeText na aktualizáciu stavovej premennej password s novou hodnotou zadanou používateľom
           onChangeText={value => {
             setPassword(value);
           }}
         />
-        {/* -- Pozadie Button komponentu nefunguje, preto použijeme Pressable
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+          {/*-- Switch komponent na rozhodnutie, či chce používateľ zostať prihlásený alebo nie --*/}
+          <Switch
+            value={shouldKeepLoggedIn} // Aktuálna hodnota switch (true - zapnuté, false - vypnuté)
+            onValueChange={value => setShouldKeepLoggedIn(value)} // Pri zmene hodnoty switchu sa aktualizuje aj stavová premenná
+          />
+          <Text>Zapamätať prihlásenie</Text>
+        </View>
+        {/* -- Tlačidlo komponent nepodporuje zmenu farby pozadia, preto použijeme Pressable
             <Button
-              title={'Submit'}
+              title={'Odoslať'}
               color={'red'}
               style={{backgroundColor: 'black'}}
             />
         */}
-        <Pressable
-          // Nastavenie štýlu Pressable na čiernu farbu pozadia
-          // Nastavenie opacity na 0.5, ak chýba email alebo heslo, alebo ak je heslo kratšie ako 8 znakov
+        <TouchableOpacity
+          // Štýl Pressable na čierne pozadie
+          // Opacity na 0.5, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
           style={[
             {backgroundColor: 'black'},
             (email.length === 0 || password.length < 8) && {opacity: 0.5},
           ]}
-          // Znefunkčnenie Pressable, ak chýba email alebo heslo, alebo ak je heslo kratšie ako 8 znakov
+          // Pressable, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
           disabled={email.length === 0 || password.length < 8}
-          // Nastavenie funkcie onPress na vypísanie hodnôt emailu a hesla do konzoly
+          // Funkcia onPress, ktorá vypíše email, heslo a hodnotu "nechať prihláseného" do konzoly
           onPress={() => {
             console.log('kliknuté');
-            console.log(email, password);
+            console.log(email, password, shouldKeepLoggedIn);
           }}>
           <Text style={{color: 'white', textAlign: 'center', padding: 10}}>
-            Submit
+            Odoslať
           </Text>
-        </Pressable>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
