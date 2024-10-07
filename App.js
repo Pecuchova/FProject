fd496a4768e2bf65da211538ef6f2a76786eb2c9
Pecuchova <jpecuchova96@gmail.com>
2024-10-07 21:22:31 +0200

    button component & form submission

--- App.js
+++ App.js
@@ -1,41 +1,75 @@
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView, Image} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+  // Button,
+  Pressable,
+  Text,
+} from 'react-native';
 
 const App = () => {
+  // Deklarácia stavových premenných na uloženie hodnôt emailu a hesla zadaných používateľom
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <SafeAreaView>
-      <ScrollView
-        // Funkcia onScroll na zobrazenie správy v konzole pri scrollovaní
-        onScroll={() => console.log('We are now scrolling')}
-        // Nastavenie obsahu na červené pozadie s pevnou výškou 600
-        contentContainerStyle={{backgroundColor: 'red', height: 600}}
-        // Skrytie vertikálneho indikátora scrollovania
-        showsVerticalScrollIndicator={false}
-        // Povolenie horizontálneho scrollovania
-        horizontal={true}
-        // Skrytie horizontálneho indikátora scrollovania
-        showsHorizontalScrollIndicator={false}>
-        <Image
-          // Zdroj obrázka 
-          source={require('./assets/images/cake.png')}
-          // Nastavenie výšky a šírky obrázka na 500
-          style={{height: 500, width: 500}}
+      <ScrollView>
+        <TextInput
+          // Nastavenie hodnoty TextInput na stavovú premennú email
+          value={email}
+          // Nastavenie keyboardType na 'email-address' pre zobrazenie klávesnice optimalizovanej pre zadávanie emailu
+          keyboardType={'email-address'}
+          // Nastavenie štýlu TextInput s ohraničením, paddingom a zaoblenými rohmi
+          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
+          // Nastavenie textu v placeholderi, ktorý vyzýva používateľa na zadanie emailu
+          placeholder={'Prosím, zadajte svoj email'}
+          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej email novou hodnotou zadanou používateľom
+          onChangeText={value => {
+            setEmail(value);
+          }}
         />
-        <Image
-          // Zdroj obrázka 
-          source={require('./assets/images/cake.png')}
-          // Nastavenie výšky a šírky obrázka na 500
-          style={{height: 500, width: 500}}
+        <TextInput
+          // Nastavenie hodnoty TextInput na stavovú premennú password (heslo)
+          value={password}
+          // Nastavenie secureTextEntry na true, aby sa zadaný text zobrazoval ako bodky (pre heslá)
+          secureTextEntry={true}
+          // Nastavenie štýlu TextInput s ohraničením, paddingom a zaoblenými rohmi
+          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
+          // Nastavenie textu v placeholderi, ktorý vyzýva používateľa na zadanie hesla
+          placeholder={'Prosím, zadajte svoje heslo'}
+          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej password novou hodnotou zadanou používateľom
+          onChangeText={value => {
+            setPassword(value);
+          }}
         />
-        <Image
-          // Zdroj obrázka 
-          source={require('./assets/images/cake.png')}
-          // Nastavenie výšky a šírky obrázka na 500
-          style={{height: 500, width: 500}}
-        />
+        {/* -- Pozadie Button komponentu nefunguje, preto použijeme Pressable
+            <Button
+              title={'Submit'}
+              color={'red'}
+              style={{backgroundColor: 'black'}}
+            />
+        */}
+        <Pressable
+          // Nastavenie štýlu Pressable na čiernu farbu pozadia
+          // Nastavenie opacity na 0.5, ak chýba email alebo heslo, alebo ak je heslo kratšie ako 8 znakov
+          style={[
+            {backgroundColor: 'black'},
+            (email.length === 0 || password.length < 8) && {opacity: 0.5},
+          ]}
+          // Znefunkčnenie Pressable, ak chýba email alebo heslo, alebo ak je heslo kratšie ako 8 znakov
+          disabled={email.length === 0 || password.length < 8}
+          // Nastavenie funkcie onPress na vypísanie hodnôt emailu a hesla do konzoly
+          onPress={() => {
+            console.log('kliknuté');
+            console.log(email, password);
+          }}>
+          <Text style={{color: 'white', textAlign: 'center', padding: 10}}>
+            Submit
+          </Text>
+        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
 export default App;
