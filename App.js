c8e7cf692858aa22f78d1b3b8a4fd86d1821975d
Pecuchova <jpecuchova96@gmail.com>
2024-10-07 22:00:08 +0200

    Flexbox

--- App.js
+++ App.js
@@ -1,110 +1,42 @@
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  TextInput,
-  // Button,
-  TouchableOpacity,
-  Switch,
-  Text,
-  View,
-} from 'react-native';
-// Importy pre FontAwesome
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faCheck} from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import {View} from 'react-native';
 
 const App = () => {
-  // Deklarácia stavových premenných pre uchovávanie hodnôt emailu a hesla, ktoré zadá používateľ
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  // Deklarácia stavovej premennej pre uchovávanie hodnoty, či používateľ má zostať prihlásený
-  const [shouldKeepLoggedIn, setShouldKeepLoggedIn] = useState(true);
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <TextInput
-          // Nastavenie hodnoty TextInput na stavovú premennú email
-          value={email}
-          // Nastavenie keyboardType na 'email-address' pre zobrazenie klávesnice na zadávanie emailov
-          keyboardType={'email-address'}
-          // Nastavenie štýlu TextInput tak, aby mal okraj, padding a zaoblené rohy
-          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
-          // Nastavenie zástupného textu (placeholder) pre emailové pole
-          placeholder={'Prosím, zadajte svoj email'}
-          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej email novou hodnotou zadanou používateľom
-          onChangeText={value => {
-            setEmail(value);
-          }}
-        />
-        <TextInput
-          // Nastavenie hodnoty TextInput na stavovú premennú heslo
-          value={password}
-          // Nastavenie secureTextEntry na true, aby sa skryté znaky zobrazovali ako body (pre heslá)
-          secureTextEntry={true}
-          // Nastavenie štýlu TextInput tak, aby mal okraj, padding a zaoblené rohy
-          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
-          // Nastavenie zástupného textu (placeholder) pre heslo
-          placeholder={'Prosím, zadajte svoje heslo'}
-          // Nastavenie funkcie onChangeText na aktualizáciu stavovej premennej heslo novou hodnotou zadanou používateľom
-          onChangeText={value => {
-            setPassword(value);
-          }}
-        />
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-          {/* -- Komponent Switch pre rozhodnutie, či má používateľ zostať prihlásený alebo nie -- */}
-          <Switch
-            value={shouldKeepLoggedIn} //Aktuálna hodnota prepínača (true - zapnuté, false - vypnuté)
-            onValueChange={value => setShouldKeepLoggedIn(value)} //Po zmene hodnoty prepínača sa obnoví stavová premenná
-          />
-          <Text>Zapamätať prihlásenie</Text>
-        </View>
-        {/* -- Button komponent nemá podporu pre background color, preto používame Pressable (TouchableOpacity)
-            <Button
-              title={'Odoslať'}
-              color={'red'}
-              style={{backgroundColor: 'black'}}
-            />
-        */}
-        <TouchableOpacity
-          // Nastavenie štýlu TouchableOpacity na čierne pozadie
-          // Nastavenie opacity na 0.5, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
-          style={[
-            {backgroundColor: 'black'},
-            (email.length === 0 || password.length < 8) && {opacity: 0.5},
-          ]}
-          // Deaktivovanie tlačidla, ak chýba email alebo heslo, alebo ak heslo má menej ako 8 znakov
-          disabled={email.length === 0 || password.length < 8}
-          // Nastavenie funkcie onPress na výpis emailu, hesla a hodnoty "nechať prihláseného" do konzoly
-          onPress={() => {
-            console.log('kliknuté');
-            console.log(email, password, shouldKeepLoggedIn);
-          }}>
-          <View
-            // Nastavenie štýlu pre View tak, aby mal flex hodnotu 1,
-            // zarovnanie komponentov v riadku (row) a centrované vertikálne aj horizontálne zarovnanie
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text
-              // Nastavenie štýlu Text tak, aby mal bielu farbu, zarovnanie na stred a padding
-              style={{color: 'white', textAlign: 'center', padding: 10}}>
-              Odoslať
-            </Text>
-            <FontAwesomeIcon
-              // Nastavenie ikony komponentu FontAwesomeIcon na ikonu faCheck
-              icon={faCheck}
-              // Nastavenie farby ikony na bielu
-              style={{color: 'white'}}
-            />
-          </View>
-        </TouchableOpacity>
-      </ScrollView>
-    </SafeAreaView>
+    <View
+      // Set the style of the View to have a flex of 1 and column direction
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alinItems: 'stretch',
+        // Possible values for flexDirection are 'row', 'row-reverse', 'column', and 'column-reverse'
+        // Flex direction determines the primary axis of the layout, either horizontal (row) or vertical (column)
+        // If 'row', items are positioned horizontally with the main axis running from left to right
+        // If 'column', items are positioned vertically with the main axis running from top to bottom
+        // If 'row-reverse', items are positioned horizontally with the main axis running from right to left
+        // If 'column-reverse', items are positioned vertically with the main axis running from bottom to top
+      }}>
+      {/* Each child View component will be placed below the previous one because flexDirection is set to 'column' */}
+      <View style={{backgroundColor: 'yellow', width: 50}} />
+      <View style={{backgroundColor: 'green', width: 50}} />
+      <View style={{backgroundColor: 'black', width: 50}} />
+      {/* justifyContent determines the alignment of items along the main axis */}
+      {/* Possible values for justifyContent are 'flex-start', 'flex-end', 'center', 'space-between', and 'space-around' */}
+      {/* If 'flex-start', items are aligned at the beginning of the main axis */}
+      {/* If 'flex-end', items are aligned at the end of the main axis */}
+      {/* If 'center', items are centered along the main axis */}
+      {/* If 'space-between', items are evenly distributed along the main axis with equal space between them */}
+      {/* If 'space-around', items are evenly distributed along the main axis with equal space around them */}
+      {/* alignItems determines the alignment of items along the cross axis */}
+      {/* Possible values for alignItems are 'flex-start', 'flex-end', 'center', 'stretch', 'baseline' */}
+      {/* If 'flex-start', items are aligned at the beginning of the cross axis */}
+      {/* If 'flex-end', items are aligned at the end of the cross axis */}
+      {/* If 'center', items are centered along the cross axis */}
+      {/* If 'stretch', items are stretched to fill the container along the cross axis */}
+      {/* If 'baseline', items are aligned at the baseline of their text */}
+    </View>
   );
 };
-
 
 export default App;
